9a903156a2d38be660d15d338f09271984423749
182182basitAliAhmad <46881502+182182basitAliAhmad@users.noreply.github.com>
2019-01-21 10:30:34 +0500

    Add files via upload

---
+++ q1_182182.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	int num = 0;
+	cin >> num;
+	for (int i = 1; i <= num; i++)
+	{
+		for (int j = 0; j < i; j++)
+			cout << i;
+		for (int j = 0; j < 2*(num-i); j++)
+			cout << "*";
+		for (int j = 0; j < i; j++)
+			cout << i;
+
+		cout << endl;
+	}
+	for (int i = 1; i < num; i++)
+	{
+		for (int j = 0; j < num-i; j++)
+			cout << i + num;
+		for (int j = 0; j < 2*i; j++)
+			cout << "*";
+		for (int j = 0; j < num - i; j++)
+			cout << i + num;
+
+		cout << endl;
+	}
+
+
+}
+
